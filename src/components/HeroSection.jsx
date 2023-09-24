2c3e56528937e6f28557d79db4bea4b41d0cc570
dreidev_04 <tacujan.andrei@gmail.com>
2023-09-24 21:12:24 +0800

    fix pngs

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -13,31 +13,31 @@ function HeroSection() {
         </h1>
         
         <h2 className="py-4 text-start">Step 1</h2>
-        <Cards image={"./src/assets/SKLauncherPage.png"} text={textContent} />
+        <Cards image={"/src/assets/SKLauncherPage.png"} text={textContent} />
 
         <h2 className="py-4 text-start">Step 2</h2>
-        <Cards image={"./src/assets/DownloadSK.png"} text={"Download the Windows \"exe\"  file"} />
+        <Cards image={"/src/assets/DownloadSK.png"} text={"Download the Windows \"exe\"  file"} />
 
         <h2 className="py-4 text-start">Step 3</h2>
-        <Cards image={"./src/assets/save.png"} text={"Save to desired location"} />
+        <Cards image={"/src/assets/save.png"} text={"Save to desired location"} />
 
         <h2 className="py-4 text-start">Step 4</h2>
-        <Cards image={"./src/assets/LaunchApp.png"} text={"Launch the app then choose offline mode"} />
+        <Cards image={"/src/assets/LaunchApp.png"} text={"Launch the app then choose offline mode"} />
 
         <h2 className="py-4 text-start">Step 5</h2>
-        <Cards image={"./src/assets/OfflineMode.png"} text={"Set your username to TeamName_Username. Then Login Offline"} />
+        <Cards image={"/src/assets/OfflineMode.png"} text={"Set your username to TeamName_Username. Then Login Offline"} />
 
         <h2 className="py-4 text-start">Step 6</h2>
-        <Cards image={"./src/assets/PlusManager.png"} text={"Click the \"+\" sign beside Installation Manager"} />
+        <Cards image={"/src/assets/PlusManager.png"} text={"Click the \"+\" sign beside Installation Manager"} />
 
         <h2 className="py-4 text-start">Step 7</h2>
-        <Cards image={"./src/assets/choose120.png"} text={"Name it to Minecraft ver 1.20 then choose the version Release: 1.20 then save"} />
+        <Cards image={"/src/assets/choose120.png"} text={"Name it to Minecraft ver 1.20 then choose the version Release: 1.20 then save"} />
 
         <h2 className="py-4 text-start">Step 7</h2>
-        <Cards image={"./src/assets/play.png"} text={"Click the downloader version then Play"} />
+        <Cards image={"/src/assets/play.png"} text={"Click the downloader version then Play"} />
 
         <h2 className="py-4 text-start">Step 8</h2>
-        <Cards image={"./src/assets/minecraft.png"} text={"HAHAHEHE Ang galing mo!"} />
+        <Cards image={"/src/assets/minecraft.png"} text={"HAHAHEHE Ang galing mo!"} />
 
       </div>
     </section>
